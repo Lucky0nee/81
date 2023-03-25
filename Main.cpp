a437f1771ebd03c796f700602aafab2d73f8bb07
Lucky0nee <65708257+Lucky0nee@users.noreply.github.com>
2023-03-25 21:03:56 +0200

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,37 @@
+#include <list>
+#include <ctime>
+#include <iostream>
+
+int main() {
+	srand(time(NULL));
+	std::list <int> list1;
+	std::list <int> list2;
+
+	std::cout << "\nlist 1:";
+	for (int i = 0; i < 100; i++) {
+		list1.push_back(rand() % 100);
+		std::cout << list1.back() << " ";
+	}
+
+	std::cout << "\nlist 2:";
+	for (int i = 0; i < list1.size(); i++) {
+		if (list1.back() % 7 == 0) {
+			list2.push_back(list1.back());
+			std::cout << list2.back() << " ";
+		}
+
+		list1.pop_back();
+	}
+
+	return 0;
+}
+
+/*
+Створіть список list <int> list1, який містить 100 випадкових натуральних чисел. На
+його основі створіть новий список, який містить тільки ті числа, які без остачі
+діляться на 7. 
+
+Після цього обчисліть співвідношення розміра початкового списка до
+розміра новоствореного.
+Початковий і кінцевий списки, а також їх розміри, виведіть на екран.
+*/
